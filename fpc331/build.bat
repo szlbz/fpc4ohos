46d6aa644c11c34337da500cb5b7d5b42ef6d048
axcom <axexe@163.com>
2026-06-03 10:40:50 +0800

    fix build

--- fpc331/build.bat
+++ fpc331/build.bat
@@ -44,9 +44,9 @@ REM 编译原生 FPC：原生编译器（ppcx64.exe） + 原生 RTL（x86_64-win64）
 make clean all install INSTALL_PREFIX=%FPCBASE%\fpc FPC=ppcx64
 
 REM 符号链接：需要管理员权限运行
-mklink "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-clang.exe"    "%LLVMDIR%\bin\clang.exe" 
-@copy batchlauncher.exe "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-as.exe"
-@echo "%LLVMDIR%\bin\clang.exe" -c -x assembler --target=%OHOS_ARCH%-linux-ohos %%* > "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-as.exe.bat"
+mklink "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-clang.exe" "%LLVMDIR%\bin\clang.exe" 
+@copy /y batchlauncher.exe "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-as.exe"
+@echo "%LLVMDIR%\bin\clang.exe" -c -x assembler --target=%OHOS_ARCH%-linux-ohos --sysroot=%SYSROOT% %%* > "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-as.exe.bat"
 mklink "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-ld.exe"    "%LLVMDIR%\bin\ld.lld.exe" 
 mklink "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-ar.exe"    "%LLVMDIR%\bin\llvm-ar.exe"
 mklink "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-nm.exe"    "%LLVMDIR%\bin\llvm-nm.exe"
@@ -55,7 +55,7 @@ mklink "%FPCBASE%\fpc\bin\x86_64-win64\%OHOS_ARCH%-ohos-strip.exe" "%LLVMDIR%\bi
 
 
 REM 构建交叉编译器
-make compiler_cycle OS_TARGET=ohos CPU_TARGET=%OHOS_ARCH% FPC=ppcx64 CROSSINSTALL=1 OPT="-O- -fPIC -O2" CROSSOPT=" -O- -Sg -Cg -O2 -Ur -dohos -Fl%SYSROOT%/usr/lib -k--sysroot=%SYSROOT%" INSTALL_PREFIX=%FPCBASE%\fpc AS="%LLVMDIR%/bin/clang.exe -c -x assembler --sysroot=%SYSROOT%" 
+make compiler_cycle OS_TARGET=ohos CPU_TARGET=%OHOS_ARCH% FPC=ppcx64 CROSSINSTALL=1 OPT="-O- -fPIC -O2" CROSSOPT=" -O- -Sg -Cg -O2 -Ur -dohos -Fl%SYSROOT%/usr/lib -k--sysroot=%SYSROOT%" INSTALL_PREFIX=%FPCBASE%\fpc 
  
 REM 编译交叉 FPC + OHOS RTL（win64 → ohos）：交叉编译器（ppcrossx64.exe） + 交叉 RTL（x86_64-ohos）
-make crossinstall OS_TARGET=ohos CPU_TARGET=%OHOS_ARCH% OPT="-O- -fPIC -O2" CROSSOPT=" -O- -Sg -Cg -O2 -Ur -dohos -Fl%SYSROOT%/usr/lib -k--sysroot=%SYSROOT%" INSTALL_PREFIX=%FPCBASE%\fpc AS="%LLVMDIR%/bin/clang.exe -c -x assembler --sysroot=%SYSROOT%" 
+make crossinstall OS_TARGET=ohos CPU_TARGET=%OHOS_ARCH% OPT="-O- -fPIC -O2" CROSSOPT=" -O- -Sg -Cg -O2 -Ur -dohos -Fl%SYSROOT%/usr/lib -k--sysroot=%SYSROOT%" INSTALL_PREFIX=%FPCBASE%\fpc 
